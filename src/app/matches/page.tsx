1ea8d6b636ebd9ddcc2b518e7fd58a26ece0dfc7
Alexei Rusu <rusualexei1995@gmail.com>
2024-02-08 21:51:26 -0500

    swiper implementation

--- src/app/matches/page.tsx
+++ src/app/matches/page.tsx
@@ -1,8 +1,18 @@
+"use client"
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { lawyerData } from '@/data/slides'
+import { Navigation, Pagination } from 'swiper/modules'
+import { useState } from 'react'
 
-function Matches() {
+
+
+ function Matches() {
+const [data, setData] = useState(lawyerData)
+
+
+
   return (
     <div className='w-full h-full bg-[#382e3c] relative flex align-middle justify-center place-content-center place-items-center'>
       <Link href='/' className='absolute left-8 top-10'>
@@ -10,13 +20,29 @@ function Matches() {
       </Link>
       <div>
         <h1 className='text-white text-4xl font-medium'>Your Matches</h1>
-        <div>
-          {/* cards */}
-          <div>
-            <button></button>
-            <button></button>
-          </div>
+        <div className='container'>
+          <Swiper
+          navigation
+          pagination={{ type: 'fraction'}}
+          modules={[Navigation, Pagination]}
+          className='bg-[#382e3c] mt-10 w-fit h-fit' 
+          >
+            {data.map((lawyer, index) => (
+              <SwiperSlide key={lawyer.id}>
+                <div className='w-80 h-96 bg-white bg-opacity-30 rounded-2xl flex flex-col justify-center place-items-center'>
+                  <Image src={lawyer.image} width={80} height={80} alt='lawyer' />
+                  <h3 className='text-white text-2xl font-medium'>{lawyer.name}</h3>
+
+                </div>
+              </SwiperSlide>
+            ))}
+            <div>
+              <button></button>
+              <button></button>
+            </div>
+          </Swiper>
         </div>
+
       </div>
       
     </div>
---
+++ src/data/slides.ts
@@ -0,0 +1,26 @@
+export const lawyerData = [
+    {
+        "id": 1,
+        "image": "/profileImage-1.png",
+        "name": "John Doe",
+        "profession": "Lawyer",
+        "services": ["Full Service Basketball", "10+ Years Experience", "Endorsements from other clients"]     
+
+    },
+    {
+        "id": 2,
+        "image": "/profileImage-2.png",
+        "name": "John Doe",
+        "profession": "Lawyer",
+        "services": ["Full Service Basketball", "10+ Years Experience", "Endorsements from other clients"]     
+
+    },
+    {
+        "id": 3,
+        "image": "/profileImage-1.png",
+        "name": "John Doe",
+        "profession": "Lawyer",
+        "services": ["Full Service Basketball", "10+ Years Experience", "Endorsements from other clients"]     
+
+    }
+]
