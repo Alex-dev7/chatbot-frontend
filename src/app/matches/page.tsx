4919b2b91da1b05b672efaadee160fe4f7777dd5
Alexei Rusu <rusualexei1995@gmail.com>
2024-02-08 22:28:08 -0500

    swipe effect

--- src/app/matches/page.tsx
+++ src/app/matches/page.tsx
@@ -3,10 +3,16 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { lawyerData } from '@/data/slides'
-import { Navigation, Pagination } from 'swiper/modules'
+
 import { useState } from 'react'
 
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/effect-cards' 
+import 'swiper/css/effect-creative'
 
+import { Navigation, Pagination, EffectCards, EffectCreative } from 'swiper/modules'
 
  function Matches() {
 const [data, setData] = useState(lawyerData)
@@ -14,32 +20,42 @@ const [data, setData] = useState(lawyerData)
 
 
   return (
-    <div className='w-full h-full bg-[#382e3c] relative flex align-middle justify-center place-content-center place-items-center'>
+    <div className='w-[430px] h-[932px] bg-[#382e3c] relative flex align-middle justify-center place-content-center place-items-center'>
       <Link href='/' className='absolute left-8 top-10'>
         <Image src='/back.svg' width={50} height={50} alt='back' />
       </Link>
       <div>
         <h1 className='text-white text-4xl font-medium'>Your Matches</h1>
-        <div className='container'>
+        <div className='container flex flex-col justify-center place-tems-center'>
           <Swiper
-          navigation
-          pagination={{ type: 'fraction'}}
-          modules={[Navigation, Pagination]}
-          className='bg-[#382e3c] mt-10 w-fit h-fit' 
+          effect={'creative'}
+          grabCursor={true}
+          creativeEffect={{
+            prev: {
+              shadow: true,
+              translate: ['-150%', 0, -900],
+            },
+            next: {
+              shadow: true,
+              translate: ['150%', 0, -900],
+            },
+          }}
+          // pagination={{ type: 'fraction'}}
+          modules={[ EffectCreative]}
+          className='bg-[#382e3c] mt-10 w-96 h-[400px] ' 
           >
             {data.map((lawyer, index) => (
-              <SwiperSlide key={lawyer.id}>
-                <div className='w-80 h-96 bg-white bg-opacity-30 rounded-2xl flex flex-col justify-center place-items-center'>
+              <SwiperSlide key={lawyer.id} >
+                <div className='w-80 h-full bg-white  rounded-2xl'>
                   <Image src={lawyer.image} width={80} height={80} alt='lawyer' />
                   <h3 className='text-white text-2xl font-medium'>{lawyer.name}</h3>
-
+                  <div>
+                    <button></button>
+                    <button></button>
+                  </div>
                 </div>
               </SwiperSlide>
             ))}
-            <div>
-              <button></button>
-              <button></button>
-            </div>
           </Swiper>
         </div>
 
