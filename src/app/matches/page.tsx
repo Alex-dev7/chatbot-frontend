a709592a5c3ccca289fb8a70988a95fc9fb41fda
Alexei Rusu <rusualexei1995@gmail.com>
2024-02-10 19:59:47 -0500

    new slidder impl

--- src/app/matches/page.tsx
+++ src/app/matches/page.tsx
@@ -1,22 +1,61 @@
 "use client"
 import Image from 'next/image'
 import Link from 'next/link'
-import { Swiper, SwiperSlide } from 'swiper/react'
 import { lawyerData } from '@/data/slides'
-
+import {  useMotionValue, useTransform, useAnimation, motion } from "framer";
 import { useState } from 'react'
 
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import 'swiper/css/effect-cards' 
-import 'swiper/css/effect-creative'
 
-import { Navigation, Pagination, EffectCards, EffectCreative } from 'swiper/modules'
 
  function Matches() {
-const [data, setData] = useState(lawyerData)
+  const [data, setData] = useState(lawyerData)
 
+   // To move the card as the user drags the cursor
+  const motionValue = useMotionValue(0);
+  // Framer animation hook
+  const animControls = useAnimation();
+
+  // To rotate the card as the card moves on drag
+  const rotateValue = useTransform(motionValue, [-200, 200], [-50, 50]);
+    // To decrease opacity of the card when swiped
+    // on dragging card to left(-200) or right(200)
+    // opacity gradually changes to 0
+    // and when the card is in center opacity = 1
+  const opacityValue = useTransform(
+      motionValue,
+      [-200, -150, 0, 150, 200],
+      [0, 1, 1, 1, 0]
+  );
+
+
+  const Card = (
+    <div className='App'>
+    <motion.div
+        // center={true}
+        // Card can be drag only on x-axis
+        drag='x'
+        _dragX={motionValue}
+        // rotate={rotateValue}
+        // opacity={opacityValue}
+        dragConstraints={{ left: -1000, right: 1000 }}
+        onDragEnd={(event, info) => {
+
+            // If the card is dragged only upto 150 on x-axis
+            // bring it back to initial position
+            if (Math.abs(info.point.x) <= 150) {
+                animControls.start({ x: 0 });
+            } else {
+
+                // If card is dragged beyond 150
+                // make it disappear
+
+                // Making use of ternary operator
+                animControls.start({ x: info.point.x < 0 ? -200 : 200 });
+            }
+        }}
+    />
+</div>
+  )
 
 
   return (
@@ -26,38 +65,42 @@ const [data, setData] = useState(lawyerData)
       </Link>
       <div>
         <h1 className='text-white text-4xl font-medium'>Your Matches</h1>
-        <div className='container flex flex-col justify-center place-tems-center'>
-          <Swiper
-          effect={'creative'}
-          grabCursor={true}
-          creativeEffect={{
-            prev: {
-              shadow: true,
-              translate: ['-150%', 0, -900],
-            },
-            next: {
-              shadow: true,
-              translate: ['150%', 0, -900],
-            },
-          }}
-          // pagination={{ type: 'fraction'}}
-          modules={[ EffectCreative]}
-          className='bg-[#382e3c] mt-10 w-96 h-[400px] ' 
-          >
+        <motion.div
+            drag='x'
+            _dragX={motionValue}
+                // transition={{ rotate: rotateValue, opacity: opacityValue}}
+                // opacity={opacityValue}
+                animate={animControls}
+
+        className='container w-96 h-[400px] mx-auto flex relative'>
             {data.map((lawyer, index) => (
-              <SwiperSlide key={lawyer.id} >
-                <div className='w-80 h-full bg-white  rounded-2xl'>
+              
+                <motion.div
+                key={lawyer.id}
+                dragConstraints={{ left: -1000, right: 1000 }}
+                onDragEnd={(event, info) => {
+                  // If the card is dragged only upto 150 on x-axis
+                  // bring it back to initial position
+                  if (Math.abs(info.point.x) <= 150) {
+                      animControls.start({ x: 0 });
+                  } else {
+                      // If card is dragged beyond 150
+                      // make it disappear
+                      // Making use of ternary operator
+                      animControls.start({ x: info.point.x < 0 ? -200 : 200 });
+                  }
+              }}
+                className='w-80 h-full bg-white  rounded-2xl border-2 '>
                   <Image src={lawyer.image} width={80} height={80} alt='lawyer' />
-                  <h3 className='text-white text-2xl font-medium'>{lawyer.name}</h3>
+                  <h3 className='text-white text-2xl font-medium '>{lawyer.name}</h3>
                   <div>
                     <button></button>
                     <button></button>
                   </div>
-                </div>
-              </SwiperSlide>
+                </motion.div>
+
             ))}
-          </Swiper>
-        </div>
+        </motion.div>
 
       </div>
       
